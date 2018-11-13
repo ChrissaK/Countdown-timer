728233dce6cba0a57f3afe31e810d546d44cadb2
ChrissaK <chrissa.kaza@gmail.com>
2018-11-13 18:46:46 +0000

    Changed date to what it should be ;)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 13, 2018 17:19:00").getTime();
+var countDownDate = new Date("Nov 14, 2018 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
